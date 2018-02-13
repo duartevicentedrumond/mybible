2b1c8cfeaa6354f4e4238c9072fbcd559b3d41e1
duartevicentedrumond <duartevicentedrumond@gmail.com>
2018-02-13 20:23:14 +0000

    JSON array http request

--- app/src/main/java/com/example/duart/mybible/main_menu_activity.java
+++ app/src/main/java/com/example/duart/mybible/main_menu_activity.java
@@ -1,6 +1,7 @@
 package com.example.duart.mybible;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,13 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 public class main_menu_activity extends AppCompatActivity {
@@ -26,8 +33,7 @@ public class main_menu_activity extends AppCompatActivity {
 
     private RequestQueue mRequestQueue;
 
-    private StringRequest stringRequest;
-    private String url = "http://www.mocky.io/v2/5a82e6352f00007d0074bc0c";
+    private String url = "http://home.localtunnel.me/android/query.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +60,11 @@ public class main_menu_activity extends AppCompatActivity {
 
         mRequestQueue = Volley.newRequestQueue(this);
 
-        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(String response) {
+            public void onResponse(JSONArray response) {
 
-                Log.i(TAG, "Reponse: " + response.toString());
-                textViewRequest.setText(response.toString());
+                Log.i(TAG, "Response: " + response.toString());
 
             }
         }, new Response.ErrorListener() {
@@ -71,8 +76,8 @@ public class main_menu_activity extends AppCompatActivity {
             }
         });
 
-        stringRequest.setTag(REQUESTTAG);
-        mRequestQueue.add(stringRequest);
+        jsonArrayRequest.setTag(REQUESTTAG);
+        mRequestQueue.add(jsonArrayRequest);
 
     }
 
