0f219af766d1f0b00156071710da1ac888626352
duartevicentedrumond <duartevicentedrumond@gmail.com>
2018-02-13 19:54:08 +0000

    print http request in textview

--- app/src/main/java/com/example/duart/mybible/main_menu_activity.java
+++ app/src/main/java/com/example/duart/mybible/main_menu_activity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,11 +15,14 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.w3c.dom.Text;
+
 public class main_menu_activity extends AppCompatActivity {
 
     private static final String TAG = main_menu_activity.class.getName();
     private static final String REQUESTTAG = "string request first";
     private Button btnSendRequest;
+    private TextView textViewRequest;
 
     private RequestQueue mRequestQueue;
 
@@ -33,6 +37,7 @@ public class main_menu_activity extends AppCompatActivity {
         Intent intent_main_menu = getIntent();
 
         btnSendRequest = (Button) findViewById(R.id.btn_send_request);
+        textViewRequest = (TextView) findViewById(R.id.text_view_request);
 
         btnSendRequest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +59,7 @@ public class main_menu_activity extends AppCompatActivity {
             public void onResponse(String response) {
 
                 Log.i(TAG, "Reponse: " + response.toString());
+                textViewRequest.setText(response.toString());
 
             }
         }, new Response.ErrorListener() {
