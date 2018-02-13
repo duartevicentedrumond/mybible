0cc76f9f86cc2be475cf3cca50350f8d0d31b792
duartevicentedrumond <duartevicentedrumond@gmail.com>
2018-02-13 19:47:52 +0000

    http request

--- app/src/main/java/com/example/duart/mybible/main_menu_activity.java
+++ app/src/main/java/com/example/duart/mybible/main_menu_activity.java
@@ -3,8 +3,27 @@ package com.example.duart.mybible;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class main_menu_activity extends AppCompatActivity {
+
+    private static final String TAG = main_menu_activity.class.getName();
+    private static final String REQUESTTAG = "string request first";
+    private Button btnSendRequest;
+
+    private RequestQueue mRequestQueue;
+
+    private StringRequest stringRequest;
+    private String url = "http://www.mocky.io/v2/5a82e6352f00007d0074bc0c";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +32,49 @@ public class main_menu_activity extends AppCompatActivity {
 
         Intent intent_main_menu = getIntent();
 
+        btnSendRequest = (Button) findViewById(R.id.btn_send_request);
+
+        btnSendRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sendRequestAndPrintResponse();
+
+            }
+        });
+
+    }
+
+    private void sendRequestAndPrintResponse() {
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.i(TAG, "Reponse: " + response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.i(TAG, "Error: " + error.toString());
+
+            }
+        });
+
+        stringRequest.setTag(REQUESTTAG);
+        mRequestQueue.add(stringRequest);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mRequestQueue != null){
+            mRequestQueue.cancelAll(REQUESTTAG);
+        }
     }
 }
