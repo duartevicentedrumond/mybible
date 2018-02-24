d334775faf82d2a4f72bc182752588d41b6d80ad
duartevicentedrumond <duartevicentedrumond@gmail.com>
2018-02-24 22:21:26 +0000

    added sync to new subdivisions: from MYSQL to SQLite

--- app/src/main/java/com/example/duart/mybible/Box.java
+++ app/src/main/java/com/example/duart/mybible/Box.java
@@ -71,6 +71,7 @@ public class Box extends AppCompatActivity {
 
                 case R.id.action_sync:
                     syncGetNewItemBox();
+                    syncGetNewSubdivisionBox();
                     return true;
 
                 default:
@@ -131,6 +132,56 @@ public class Box extends AppCompatActivity {
 
                         sqLiteDatabase = dataBase.getWritableDatabase();
                         sqLiteDatabase.execSQL("INSERT INTO item (id, name, category, status) VALUES (" + id + ", '" + name + "', '" + category + "', 1 );");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.i(TAG, "Error: " + error.toString());
+
+            }
+        });
+
+        jsonArrayRequest.setTag(REQUESTTAG);
+        mRequestQueue.add(jsonArrayRequest);
+
+    }
+
+    private void syncGetNewSubdivisionBox() {
+
+        /**sqLiteDatabase = dataBase.getWritableDatabase();
+         String deleteQuery = "DELETE FROM wallet;";
+         sqLiteDatabase.execSQL(deleteQuery);**/
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        String url = "http://casa.localtunnel.me/android/sync_get_new_subdivision_box_android.php";
+
+        final DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                for (int i = 0; i < response.length(); i++) {
+
+                    try {
+
+                        JSONObject mysqlDataUnsync = response.getJSONObject(i);
+
+                        Integer id = Integer.parseInt(mysqlDataUnsync.getString("id"));
+                        String subdivision = mysqlDataUnsync.getString("subdivision");
+
+                        sqLiteDatabase = dataBase.getWritableDatabase();
+                        sqLiteDatabase.execSQL("INSERT INTO subdivision (id, subdivision, status) VALUES (" + id + ", '" + subdivision + "', 1 );");
 
                     } catch (JSONException e) {
                         e.printStackTrace();
